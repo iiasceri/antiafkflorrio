01b8fe397ec377e79fb6358c395f08d9d3c45630
Nick Iascerinschi <nick.iasceri@gmail.com>
2023-06-09 21:00:11 +0300

    test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-watch yt vid
+For windows it just works for mac follow vid or read this:
+
+unpack .app file from dmg to anywhere
+right click on .app -> Show Package Contents
+go to Contents->MacOS and you will see afkflorrio right?
+just double click on it and allow all permissions like in vid
--- generateapp.sh
+++ generateapp.sh
@@ -1,17 +1,15 @@
-rm -rf build;rm -rf dist;rm -rf florrioafkmac.spec
-brew install create-dmg
-/opt/homebrew/opt/python@3.9/bin/python3.9 -m PyInstaller --name 'afkinflorrio' --icon 'icon.icns' --windowed florrioafkmac.py
-create-dmg \
-  --volname "afkinflorrio" \
-  --volicon "icon.icns" \
-  --window-pos 200 120 \
-  --window-size 600 300 \
-  --icon-size 512 \
-  --icon "afkinflorrio.app" 175 120 \
-  --hide-extension "afkinflorrio.app" \
-  --app-drop-link 425 120 \
-  "dist/afkinflorrio.dmg" \
-  "dist/dmg/"
-
+rm -rf build;rm -rf dist;rm -rf florrioafk.spec
+/opt/homebrew/opt/python@3.9/bin/python3.9 -m PyInstaller --name 'afkinflorrio' --icon 'icon.icns' --windowed florrioafk.py
+#create-dmg \
+#  --volname "afkinflorrio" \
+#  --volicon "icon.icns" \
+#  --window-pos 200 120 \
+#  --window-size 600 300 \
+#  --icon-size 512 \
+#  --icon "afkinflorrio.app" 175 120 \
+#  --hide-extension "afkinflorrio.app" \
+#  --app-drop-link 425 120 \
+#  "dist/afkinflorrio.dmg" \
+#  "dist/dmg/"
 #brew install codesign
 #codesign -fs my-new-cert --force --deep --timestamp --options runtime whattosign
